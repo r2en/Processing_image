c01866ec89077a728908d3bd63df4b71b2a14bfc
xxxHAL <s12161te@gmail.com>
2017-02-21 10:30:17 +0900

    SIFT

---
+++ SIFT/sift02.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('castel.jpg')
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+detector = cv2.AKAZE_create()
+kp = detector.detect(gray)
+
+img=cv2.drawKeypoints(gray,kp,None)
+
+cv2.imshow('sift_keypoints.jpg',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
